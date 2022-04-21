2e6db3d179f4705c7faa3ca56f42fd704c979233
Artur-Yurii Korchynskyi <artur-yurii.korchynskyi@gmail.com>
2022-04-21 23:27:07 +0300

    Returned to version 4 since it more stable

--- internal-api-server/api/tiktok.py
+++ internal-api-server/api/tiktok.py
@@ -1,6 +1,5 @@
 from TikTokApi import TikTokApi
 import json
-
 from api.social_network_api import SocialNetworkAPI
 from common.decorators import change_proxy_on_return_null
 from common.proxy_handling import PROXY_URL
@@ -8,7 +7,7 @@ from common.proxy_handling import PROXY_URL
 
 class TikTokAPI(SocialNetworkAPI):
     def __init__(self) -> None:
-        self.tiktok = TikTokApi(
+        self.tiktok = TikTokApi.get_instance(
             use_test_endgpoints=True, proxy=PROXY_URL)
         self.last_call_was_succeess = True
 
@@ -16,7 +15,7 @@ class TikTokAPI(SocialNetworkAPI):
     def user_info(self, user_id: str) -> dict:
         try:
             self.last_call_was_succeess = True
-            return self.tiktok.user(username=user_id).as_dict
+            return self.tiktok.get_user_object(user_id)
         except:
             self.last_call_was_succeess = False
             return None
@@ -28,11 +27,10 @@ class TikTokAPI(SocialNetworkAPI):
     @change_proxy_on_return_null
     def content(self, user_id: str, content_type, count: int) -> dict:
         try:
-            user = self.tiktok.user(username=user_id)
-            result = user.liked(
-                count) if content_type == "likes" else user.videos(count)
+            result = self.tiktok.user_liked_by_username(
+                user_id, count) if content_type == "likes" else self.tiktok.by_username(user_id, count)
             self.last_call_was_succeess = True
-            return json.dumps([x.as_dict for x in result])
+            return json.dumps(result)
         except:
             self.last_call_was_succeess = False
             return None
@@ -41,13 +39,10 @@ class TikTokAPI(SocialNetworkAPI):
     def content_by_id(self, content_id: str) -> dict:
         try:
             self.last_call_was_succeess = True
-            return json.dumps([self.tiktok.video(id=content_id).as_dict])
+            return json.dumps([self.tiktok.get_tiktok_by_id(id=content_id).get('itemInfo').get('itemStruct')])
         except:
             self.last_call_was_succeess = False
             return None
 
     def status(self) -> bool:
         return self.last_call_was_succeess
-
-    def __del__(self):
-        del self.tiktok
